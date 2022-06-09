5c11bc43c4a1992b642652f921e9a7a2f0e7706b
Denis Tolkachev <de-tolkac@ya.ru>
2022-06-09 03:15:05 +0300

    Made fields of HashTable visible (for tests)

--- storage/hashtable.go
+++ storage/hashtable.go
@@ -3,37 +3,37 @@ package storage
 import "sync"
 
 type HashTable struct {
-	urlToCode map[string]string
-	codeToUrl map[string]string
-	mutex     sync.Mutex
+	UrlToCode map[string]string
+	CodeToUrl map[string]string
+	Mutex     sync.Mutex
 }
 
 func (table *HashTable) Init() error {
-	table.urlToCode = make(map[string]string)
-	table.codeToUrl = make(map[string]string)
+	table.UrlToCode = make(map[string]string)
+	table.CodeToUrl = make(map[string]string)
 
 	return nil
 }
 
 func (table *HashTable) FindEncodedUrl(url string) (res string, found bool) {
-	res, found = table.codeToUrl[url]
+	res, found = table.CodeToUrl[url]
 	return
 }
 
 func (table *HashTable) FindDecodedUrl(url string) (res string, found bool) {
-	res, found = table.urlToCode[url]
+	res, found = table.UrlToCode[url]
 	return
 }
 
 func (table *HashTable) SaveUrl(decodedUrl, encodedUrl string) {
-	table.codeToUrl[decodedUrl] = encodedUrl
-	table.urlToCode[encodedUrl] = decodedUrl
+	table.CodeToUrl[decodedUrl] = encodedUrl
+	table.UrlToCode[encodedUrl] = decodedUrl
 }
 
 func (table *HashTable) Lock() {
-	table.mutex.Lock()
+	table.Mutex.Lock()
 }
 
 func (table *HashTable) Unlock() {
-	table.mutex.Unlock()
+	table.Mutex.Unlock()
 }
